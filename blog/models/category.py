075fc5c6af56c034336cf50b16750c0e84b0ef24
Valiyev Vali <valiyevveli@outlook.com>
2021-08-03 00:03:42 +0400

    category model created

---
+++ blog/models/category.py
@@ -0,0 +1,14 @@
+from enum import unique
+from django.db import models
+from autoslug import AutoSlugField
+class CategoryModel(models.Model):
+    name=models.CharField(max_length=30,blank=False,null=False)
+    slug=AutoSlugField(populate_from='name',unique=True)
+
+    class Meta:
+        db_table='Category'
+        verbose_name='Category'
+        verbose_name_plural='Categories'
+
+    def __str__(self):
+        return self.slug
