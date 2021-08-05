af1c975ef2063c18c7622aaacd0ed18fa00d140b
Valiyev Vali <valiyevveli@outlook.com>
2021-08-05 23:55:50 +0400

    account finished

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path,include
 from account.views import Logout_user,Change_Password,Edit_Profile,Register
+from django.contrib.auth import views as auth_views
 urlpatterns = [
+   path('login',auth_views.LoginView.as_view(
+      template_name='pages/login.html'
+   ),name='login'),
    path('logout',Logout_user,name='logout'),
    path('change_password',Change_Password,name='change_password'),
    path('profile_edit',Edit_Profile,name='profile_edit'),
--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,5 @@ MEDIA_URL = '/media/'
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL='/'
