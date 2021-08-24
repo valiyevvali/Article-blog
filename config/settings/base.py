7ebf4f911e7e6bc07ff067750bec9234af81aa2f
Valiyev Vali <valiyevveli@outlook.com>
2021-08-24 15:10:38 +0400

    s3 started

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'account',
     'crispy_forms',
     'django_cleanup.apps.CleanupConfig',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -181,3 +182,14 @@ sentry_sdk.init(
     traces_sample_rate=1.0,
     send_default_pii=True
 )
+
+AWS_ACCESS_KEY_ID=env('ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=env('SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME='vblogv'
+AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS={
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION='static'
+STATIC_URL= 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN,AWS_LOCATION)
+STATICFILES_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
--- config/settings/development.py
+++ config/settings/development.py
@@ -5,6 +5,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+
 AUTH_PASSWORD_VALIDATORS = [
    
 ]
